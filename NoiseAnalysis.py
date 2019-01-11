43686e890fe9fbb99514e9de916058895a7304b6
Chen Pei Hao <s894117@gmail.com>
2019-01-11 20:21:43 +0900

    Correct the data process & complete the distribution part

--- NoiseAnalysis.py
+++ NoiseAnalysis.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from errorEstimation import csv2data, add_noise, RSS_cal, estimate_distacne, random_test
+from errorEstimation import csv2data, add_noise, RSS_cal, estimate_distacne, random_test, draw_histogram
 from os import listdir
 from os.path import isfile, join
+from tqdm import tqdm
 
 STD = 0.1
 def numpy_sort(np_array, arg):
@@ -22,16 +23,22 @@ def test():
 
 def positionDataProcess():
     outdata = {}
-    data = csv2data("RSSdata.csv")
+    data = csv2data("result/RSSdata.csv")
     data = numpy_sort(data, 0)
     row, col = data.shape
-    for i in range(row):
-        index = str(round(data[i, 0], 1))
-        if index not in outdata:
-            outdata[index] = []
-            outdata[index].append(data[i, 1::].tolist())
-        else:
-            outdata[index].append(data[i, 1::].tolist())
+    for i in tqdm(range(row),ncols=100, desc="Progress"):
+        on_signal = data[0:3]
+        off_signal = data[3:6]
+        origin = np.array([[RSS_cal(on_signal, off_signal)]])
+        origin_result = estimate_distacne(origin)
+        if abs(origin_result - data[i, 0]) < 2:
+            index = str(round(data[i, 0], 1))
+            if index not in outdata:
+                outdata[index] = []
+                outdata[index].append(data[i, 1::].tolist())
+            else:
+                outdata[index].append(data[i, 1::].tolist())
+
     for key, value in outdata.items():
         key = key.replace(".", "_")
         with open("positionData/D{}.csv".format(key), "w") as f:
@@ -66,14 +73,39 @@ def test_distribution():
 def path_file_list(path="positionData"):    
     return [f for f in listdir(path) if isfile(join(path, f))]
 
+def draw_distribution(test_time):
+    file_name = "D57_5"
+    data = csv2data("positionData/{}.csv".format(file_name))
+    ideal = processDistance(file_name)
+    row, col = data.shape
+    times = int(test_time / row)
+    result_bucket = []
+    for i in range(times):
+        for item in data:
+            on_signal = item[0:3]
+            off_signal = item[3:6]
+            noise_on_signal = add_noise(on_signal)
+            noise_off_signal = add_noise(off_signal)
+            noise_RSS = np.array([[RSS_cal(noise_on_signal, noise_off_signal)]])
+            noise_result = estimate_distacne(noise_RSS)
+            result_bucket.append(noise_result)
+    result_bucket = np.asarray(result_bucket)
+    draw_histogram(result_bucket, xlabel="Estimated Distance")
 
+        # noise_on_signal = add_noise(on_signal)
+        # noise_off_signal = add_noise(off_signal)
+        # noise_RSS = np.array([[RSS_cal(noise_on_signal, noise_off_signal)]])
+
+        # noise_result = estimate_distacne(noise_RSS)
+        # print("Origin : {}, Result: {}".format(origin_result, noise_result))
 
 
 
         
 if __name__ == '__main__':
     # test()
-    random_test(10000)
+    # random_test(10000)
     positionDataProcess()
-    test_distribution()
+    # test_distribution()
     # test_list()
+    # draw_distribution(10000)

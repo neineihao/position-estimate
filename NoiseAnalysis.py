63fcad42d431c29ff8dabd68cdf863fdd93c29cc
p-chen <p-chen@ist.osaka-u.ac.jp>
2019-01-11 19:10:30 +0900

    Change the read file

--- NoiseAnalysis.py
+++ NoiseAnalysis.py
@@ -1,8 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from errorEstimation import csv2data, add_noise, RSS_cal, estimate_distacne
-
+from errorEstimation import csv2data, add_noise, RSS_cal, estimate_distacne, random_test
+from os import listdir
+from os.path import isfile, join
 
 STD = 0.1
 def numpy_sort(np_array, arg):
@@ -21,7 +22,7 @@ def test():
 
 def positionDataProcess():
     outdata = {}
-    data = csv2data("result/RSSdata.csv")
+    data = csv2data("RSSdata.csv")
     data = numpy_sort(data, 0)
     row, col = data.shape
     for i in range(row):
@@ -34,6 +35,7 @@ def positionDataProcess():
     for key, value in outdata.items():
         key = key.replace(".", "_")
         with open("positionData/D{}.csv".format(key), "w") as f:
+            f.write("Bxon,Byon,Bzon,Bxoff,Byoff,Bzoff\n")
             for item in value:
                 f.write("{},{},{},{},{},{}\n".format(item[0], item[1], item[2], item[3], item[4], item[5]))
 
@@ -43,34 +45,35 @@ def processDistance(filename):
     distance = distance.replace("_", ".")
     return float(distance)
 
-
 def test_distribution():
-    test_file = "D100_0.csv"
-    data = csv2data("positionData/{}".format(test_file))
-    distance = processDistance(test_file)
-    for item in data:
-        on_signal = item[0:3]
-        off_signal = item[3:6]
-        origin = np.array([[RSS_cal(on_signal, off_signal)]])
-        noise_on_signal = add_noise(on_signal)
-        noise_off_signal = add_noise(off_signal)
-        noise_RSS = np.array([[RSS_cal(noise_on_signal, noise_off_signal)]])
-        origin_result = estimate_distacne(origin)
-        noise_result = estimate_distacne(noise_RSS)
-        print("Origin : {}, Result: {}".format(origin_result,noise_result) )
+    path_files = path_file_list()
+    for path_file in path_files:
+        # test_file = "D49_9.csv"
+        data = csv2data("positionData/{}".format(path_file))
+        distance = processDistance(path_file)
+        print("IN distance: {}".format(distance))
+        for item in data:
+            on_signal = item[0:3]
+            off_signal = item[3:6]
+            origin = np.array([[RSS_cal(on_signal, off_signal)]])
+            noise_on_signal = add_noise(on_signal)
+            noise_off_signal = add_noise(off_signal)
+            noise_RSS = np.array([[RSS_cal(noise_on_signal, noise_off_signal)]])
+            origin_result = estimate_distacne(origin)
+            noise_result = estimate_distacne(noise_RSS)
+            print("Origin : {}, Result: {}".format(origin_result,noise_result) )
+
+def path_file_list(path="positionData"):    
+    return [f for f in listdir(path) if isfile(join(path, f))]
 
 
 
 
 
-
-
-
-
-
-
-
+        
 if __name__ == '__main__':
     # test()
-    # positionDataProcess()
+    random_test(10000)
+    positionDataProcess()
     test_distribution()
+    # test_list()
